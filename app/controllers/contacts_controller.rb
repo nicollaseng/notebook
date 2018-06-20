ed80b13717dbca5b0a9105e2a1725e82fa235389
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-06-19 21:08:31 -0300

    kaminari i18n added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
+    @contacts = Contact.all.order(:name).page(params[:page])
   end
 
   # GET /contacts/1

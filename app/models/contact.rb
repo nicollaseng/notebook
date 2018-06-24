cfe819bc771e38130374d63df19a074d7340163f
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-06-23 23:39:58 -0300

    Whole page stylesheet changed

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,6 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_one :address
   has_many :phones
-  
-
   accepts_nested_attributes_for :address, allow_destroy: true
   accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
   validates :name, :email, presence: true
